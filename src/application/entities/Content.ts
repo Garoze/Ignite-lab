e1b3ac85a94c559efd9a369c501919c349b611bc
Garoze <marcelogaroze@hotmail.com>
2022-12-14 09:54:54 -0300

    feat: added content entity

---
+++ src/application/entities/Content.ts
@@ -0,0 +1,23 @@
+export class Content {
+  private readonly content: string;
+
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContentLength(content);
+
+    if (!isContentLengthValid) {
+      throw new Error(
+        'Content length must be greater or equal to 5 and less or equal to 250',
+      );
+    }
+
+    this.content = content;
+  }
+
+  public get value(): string {
+    return this.content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 250;
+  }
+}
