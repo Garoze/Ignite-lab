8f18457d793271855088755f8b1bde5845a223d7
Garoze <marcelogaroze@hotmail.com>
2022-12-18 16:58:49 -0300

    refactor: changed the import on data.module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { NotificationsRepository } from '../../application/repositories/NotificationsRepository';
+import { NotificationsRepository } from '@application/repositories/NotificationsRepository';
 import { PrismaNotificationsRepository } from './prisma/repositories/prismaNotificationsRepository';
 
 @Module({
