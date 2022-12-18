f39c355c5fc5091821d21703ce6585bdc2d0279a
Garoze <marcelogaroze@hotmail.com>
2022-12-18 16:59:42 -0300

    feat: in memory notification repository

--- test/repositories/inMemoryNotificationsRepository.ts
+++ test/repositories/inMemoryNotificationsRepository.ts
@@ -1,10 +1,39 @@
-import { Notification } from '../../src/application/entities/Notification';
-import { NotificationsRepository } from '../../src/application/repositories/NotificationsRepository';
+import { Notification } from '@application/entities/Notification';
+import { NotificationsRepository } from '@application/repositories/NotificationsRepository';
 
 export class inMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    return notification || null;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0)
+      this.notifications[notificationIndex] = notification;
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
   }
 }
