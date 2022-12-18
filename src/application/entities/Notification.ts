b99013151631e3a7568583e260145cb9baa09a04
Garoze <marcelogaroze@hotmail.com>
2022-12-18 16:58:29 -0300

    feat: added canceledAt
    also fixed the id

--- src/application/entities/Notification.ts
+++ src/application/entities/Notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -14,8 +15,11 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
@@ -50,12 +54,24 @@ export class Notification {
     return this.props.category;
   }
 
-  public set readAt(readAt: Date | undefined | null) {
-    this.props.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | undefined | null {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | undefined | null {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {
