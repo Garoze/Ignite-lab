38a8eafbe801b10288b23dd16ad3fec258debed6
Garoze <marcelogaroze@hotmail.com>
2022-12-18 16:59:55 -0300

    feat: prisma notifications mapper

---
+++ src/infra/database/prisma/mappers/prismaNotificationMapper.ts
@@ -0,0 +1,31 @@
+import { Notification as RawNotification } from '@prisma/client';
+import { Notification } from '@application/entities/Notification';
+import { Content } from '@application/entities/Content';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      recipientId: notification.recipientId,
+      category: notification.category,
+      content: notification.content.value,
+      readAt: notification.readAt,
+      canceledAt: notification.canceledAt,
+      createdAt: notification.createdAt,
+    };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
+  }
+}
