ce28f1ed203d5a8274092c60965aa7f68835c2ad
Garoze <marcelogaroze@hotmail.com>
2022-12-18 17:03:41 -0300

    feat: added all the providers to http.module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from '@application/useCases/CancelNotification';
+import { CountRecipientNotifications } from '@application/useCases/CountRecipientNotifications';
+import { GetRecipientNotifications } from '@application/useCases/GetRecipientNotifications';
+import { ReadNotification } from '@application/useCases/ReadNotification';
+import { UnreadNotification } from '@application/useCases/UnreadNotification';
 import { Module } from '@nestjs/common';
 import { SendNotification } from '../../application/useCases/SendNotification';
 import { DatabaseModule } from '../database/database.module';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
